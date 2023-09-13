69ade10ae8071df8631af3d898b2c2319a23ef99
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-09-13 11:39:16 -0500

    made list and sorted

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>() { "Fallout", "Halo", "Fable", "World of Warcraft" };
+            var sortedGames = videoGames.OrderBy(x => x.Length);
+            foreach (var videoGame in sortedGames)
+            {
+                Console.WriteLine(videoGame);
+            }
         }
     }
 }
